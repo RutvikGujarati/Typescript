d91a321b6e8fb59af4de5a4de3344453dc9d220d
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2023-11-23 14:04:32 +0530

    enums

---
+++ Enums.ts
@@ -0,0 +1,19 @@
+const enum After10th{
+      Science = 2,
+      Commerse,
+      Arts,
+      Deploma
+}
+
+const obj = {
+    study : 10,
+
+}
+
+const  selected = After10th.Science;
+const  selection = obj.study;
+
+console.log(selected)
+console.log(selection)
+
+export {}
---
+++ out/Enums.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var obj = {
+    study: 10,
+};
+var selected = 2 /* After10th.Science */;
+var selection = obj.study;
+console.log(selected);
+console.log(selection);
